a1a4e1698399fe9fca163d0481932bc9527f3ae3
Martin Lindhe <martin@ubique.se>
2014-04-30 16:40:10 +0200

    NEW: decodes eval(base64_encode(...))

--- PhpDeobfuscator/PhpDeobfuscator.cs
+++ PhpDeobfuscator/PhpDeobfuscator.cs
@@ -5,10 +5,16 @@ namespace PhpDeobfuscator
 {
 	public class PhpDeobfuscator
 	{
-		public static string DecodeLine (string line)
+		public static string Decode (string line)
 		{
 			// TODO only decode hex inside "" strings
 
+			var unhexed = DecodeHex (line);
+			return EvalAndBase64StringDecode (unhexed);
+		}
+
+		private static string DecodeHex (string line)
+		{
 			string findStr = @"\x";
 
 			var x = new StringBuilder ();
@@ -28,6 +34,63 @@ namespace PhpDeobfuscator
 			}
 
 			return x.ToString ();
+		}
+
+		/**
+		 * Detects and decodes eval( base64_decode() ) method in s (PHP code)
+		 */
+		private static string EvalAndBase64StringDecode (string s)
+		{
+			// find ofs of eval
+			int ofsEval = s.IndexOf ("eval");
+			if (ofsEval == -1) {
+				return s;
+			}
+
+			// find offset to base64_decode
+			int ofsBase64Deccode = s.IndexOf ("base64_decode");
+			if (ofsBase64Deccode == -1) {
+				return s;
+			}
+
+			// find ( and ) following method name
+			int brace1 = s.IndexOf ("(", ofsBase64Deccode);
+			int brace2 = s.IndexOf (")", ofsBase64Deccode);
+			if (brace1 == -1 || brace2 == -1) {
+				return s;
+			}
+
+			var base64 = s.Substring (brace1 + 1, brace2 - brace1 - 1).Trim ();
+
+			string decoded = "";
+
+			if (base64.StartsWith ("'") && base64.EndsWith ("'")) {
+				decoded = FromBase64 (base64.Substring (1, base64.Length - 2));
+			}
+
+			if (base64.StartsWith ("\"") && base64.EndsWith ("\"")) {
+				decoded = FromBase64 (base64.Substring (1, base64.Length - 2));
+			}
+
+			if (decoded.Length == 0) {
+				return s;
+			}
+
+			string before = s.Substring (0, ofsEval);
+
+			// remove closing ) and ; from eval statement
+			int closingPos = s.IndexOf (")", brace2 + 1);
+			closingPos = s.IndexOf (";", closingPos);
+
+			string after = s.Substring (closingPos + 1);
+
+			return before + decoded + after;
+		}
+
+		private static string FromBase64 (string input)
+		{
+			var x = System.Convert.FromBase64String (input);
+			return System.Text.ASCIIEncoding.UTF8.GetString (x);
 		}
 	}
 }
